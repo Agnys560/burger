5fcc305ad94caab96c3dc1acf7143878bc2bae15
Jonathan Modlin <jonathanmodlin1@gmail.com>
2020-06-27 20:34:40 -0400

    burgertronfix

--- config/connection.js
+++ config/connection.js
@@ -7,9 +7,9 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection({
     host: "qf5dic2wzyjf1x5x.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     port: 3306,
-    user: "bf6hhbp62sg9va1r",
-    password: "fpzx4srmmzy5amdr",
-    database: "ytpm1kqx2424351r"
+    user: "bf6hhbp62sg9va1e",
+    password: "fpzx4srmmzy5amdv",
+    database: "ytpm1kqx2424351k"
   });
 };
 connection.connect(function(err) {
